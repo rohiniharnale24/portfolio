ff53573280ef0b170c55a425dbadb5cd8a38ca20
rohiniharnale24 <harnaler@gmail.com>
2024-01-21 13:17:10 +0530

    added introduction component

--- src/app/repeat-array.directive.ts
+++ src/app/repeat-array.directive.ts
@@ -4,12 +4,5 @@ import { Directive, HostBinding } from '@angular/core';
   selector: '[appRepeatArray]',
 })
 export class RepeatArrayDirective {
-  displaySkills: Array<string> = ['Web Developer', 'Angular Developer'];
   constructor() {}
-  repeatElements() {
-    for (let i = 0; i < this.displaySkills.length; i++) {
-      if (this.displaySkills.length - 1) {
-      }
-    }
-  }
 }
--- src/app/shared/components/introduction/introduction.component.ts
+++ src/app/shared/components/introduction/introduction.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroductionComponent implements OnInit {
   constructor() {}
+  ngOnInit() {
+    this.currentDisplayedSkill = this.displaySkills[this.activeIndex];
 
-  ngOnInit(): void {}
+    this.loop();
+  }
+  displaySkills: Array<string> = [
+    'Web Developer',
+    'Angular Developer',
+    'Salesforce Developer',
+  ];
+  currentDisplayedSkill!: any;
+  activeIndex: number = 0;
+  loop() {
+    setTimeout(() => {
+      if (this.activeIndex == 0) {
+        this.activeIndex = this.displaySkills.length - 2;
+      } else if (this.activeIndex == 1) {
+        this.activeIndex = this.displaySkills.length - 1;
+      } else {
+        this.activeIndex = 0;
+      }
+      this.currentDisplayedSkill = this.displaySkills[this.activeIndex];
+
+      this.loop();
+      console.log('current element1', this.currentDisplayedSkill);
+    }, 5000);
+
+    console.log('current element2', this.currentDisplayedSkill);
+  }
 }
